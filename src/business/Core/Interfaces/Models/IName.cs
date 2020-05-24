f3819b12f845c7f0e2348c26ec4a9eda5150ed4b
Yasharvc <yashar@pragmateam.dev>
2020-05-24 20:33:34 +0300

    Added Stage properties

---
+++ src/business/Core/Interfaces/Models/IName.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Interfaces.Models
+{
+	public interface IName
+	{
+		public string Name { get; set; }
+	}
+}
--- src/business/Core/Models/Stage.cs
+++ src/business/Core/Models/Stage.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Core.Interfaces.Models;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Core.Models
 {
-	class Stage
+	public class Stage: Model, IName
 	{
+		public string Name { get; set; }
+		public Stage Previous { get; set; }
+		public List<Stage> Next { get; set; }
+		public string UnqueID { get; set; }
 	}
 }
