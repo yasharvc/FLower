4a4abe0d745d9207f9241c967d58af5aead55426
Yasharvc <yashar@pragmateam.dev>
2020-05-27 00:25:53 +0300

    Added ProcessTemplate for making a common base for Process generating an using

---
+++ src/business/Core/Models/ProcessTemplate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Models
+{
+	public class ProcessTemplate : Model
+	{
+		public Process Template { get; set; }
+		public string CreatorID { get; set; }
+		public DateTime CreationDateTime { get; set; }
+		public bool IsActive { get; set; }
+		public List<ProcessTemplate> History { get; set; }
+	}
+}
